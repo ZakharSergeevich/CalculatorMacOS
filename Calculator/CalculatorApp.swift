622cb34722418f76bbe70a8f8c8414f1c09983b1
ZakharGarbuzov <zakhar-garbuzov@inbox.ru>
2023-07-02 00:15:47 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Захар Гарбузов on 02.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
