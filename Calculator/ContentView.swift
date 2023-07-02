a15efdf5130e49f164725e430f24f970b2e03b7d
ZakharGarbuzov <zakhar-garbuzov@inbox.ru>
2023-07-02 20:53:07 +0300

    feat(input): nums keyboard

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,25 +7,118 @@
 
 import SwiftUI
 
+enum CalcButton: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case subtract = "-"
+    case divide = "/"
+    case multiply = "x"
+    case equal = "="
+    case clear = "AC"
+    case decimal = "."
+    case percent = "%"
+    case negative = "-/+"
+    
+    var buttonColor: Color {
+        switch self {
+        case .add, .subtract, .multiply, .divide, .equal:
+            return .orange
+        case .clear, .negative, .percent:
+            return Color(.lightGray)
+        default:
+            return .green
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    @State var value = "0"
+    
+    let buttons: [[CalcButton]] = [
+        [.clear, .negative, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .subtract],
+        [.one, .two, .three, .add],
+        [.zero, .decimal, .equal]
+    ]
+    
     var body: some View {
-        VStack {
-            ZStack {
-                Color.black.edgesIgnoringSafeArea(.all)
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 12) {
+                Spacer()
+                // Text
+                HStack {
+                    Spacer()
+                    Text(value)
+                        .bold()
+                        .font(.system(size: 100))
+                        .foregroundColor(.white)
+                }
+                .padding()
                 
-                VStack {
-                    // Text
-                    HStack {
-                        Spacer()
-                        Text("0")
-                            .bold()
-                            .font(.system(size: 64))
-                            .foregroundColor(.white)
+                // Buttons
+                ForEach(buttons, id: \.self) { row in
+                    HStack(spacing: 12) {
+                        ForEach(row, id: \.self) { item in
+                            Button(action: {
+                                self.didTap(button: item)
+                            }, label: {
+                                Text(item.rawValue)
+                                    .font(.system(size: 32))
+                                    .frame(
+                                        width: buttonWidth(item: item),
+                                        height: 70
+                                    )
+                                    .background(item.buttonColor)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(35)
+                            })
+                        }
                     }
+                    .padding(.bottom, 3)
                 }
             }
         }
-        .padding()
+    }
+    
+    func didTap(button: CalcButton) {
+        switch button {
+        case .add, .subtract, .divide, .multiply:
+            break
+        case .clear:
+            self.value = "0"
+            break
+        case .decimal, .negative, .percent:
+            break
+        default:
+            let number = button.rawValue
+            if self.value == "0" {
+                value = number
+            }
+            else {
+                self.value = "\(self.value)\(number)"
+            }
+        }
+    }
+    
+    func buttonWidth(item: CalcButton) -> CGFloat {
+        //return (XCUIScreen.main.bounds.width - (5 *  12)) / 4
+        if item == .zero {
+            return 170
+        }
+        return 70
     }
 }
 
